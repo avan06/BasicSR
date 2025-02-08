a2ba3c6973e3a4d95ee95406698237c37744d0f7
avan <sakaja_218@yahoo.com.tw>
2025-02-09 01:56:04 +0800

    rrdbnet_arch.py updated to support loading old models that are not from Real-ESRGAN.

--- basicsr/archs/rrdbnet_arch.py
+++ basicsr/archs/rrdbnet_arch.py
@@ -82,31 +82,68 @@ class RRDBNet(nn.Module):
             Default: 64
         num_block (int): Block number in the trunk network. Defaults: 23
         num_grow_ch (int): Channels for each growth. Default: 32.
+        is_real_esrgan (bool): Whether to use the Real-ESRGAN mode. Default is True.
+                               If True, adjust num_in_ch based on scale.
+                               If False, adjust conv_up2 based on scale.
     """
 
-    def __init__(self, num_in_ch, num_out_ch, scale=4, num_feat=64, num_block=23, num_grow_ch=32):
+    def __init__(self, num_in_ch, num_out_ch, scale=4, num_feat=64, num_block=23, num_grow_ch=32, is_real_esrgan=True):
         super(RRDBNet, self).__init__()
         self.scale = scale
-        if scale == 2:
-            num_in_ch = num_in_ch * 4
-        elif scale == 1:
-            num_in_ch = num_in_ch * 16
+        self.num_block = num_block
+        self.is_real_esrgan = is_real_esrgan
+        if is_real_esrgan:
+            if scale == 2:
+                num_in_ch = num_in_ch * 4
+            elif scale == 1:
+                num_in_ch = num_in_ch * 16
         self.conv_first = nn.Conv2d(num_in_ch, num_feat, 3, 1, 1)
         self.body = make_layer(RRDB, num_block, num_feat=num_feat, num_grow_ch=num_grow_ch)
         self.conv_body = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
         # upsample
         self.conv_up1 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
-        self.conv_up2 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+        self.conv_up2 = nn.Conv2d(num_feat, num_feat, 3, 1, 1) if is_real_esrgan or scale == 4 else None
         self.conv_hr = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
         self.conv_last = nn.Conv2d(num_feat, num_out_ch, 3, 1, 1)
 
         self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
+    def load_state_dict(self, state_dict, strict=True):
+        if "conv_first" in state_dict:
+            return super().load_state_dict(state_dict, strict=strict)
+            
+        loadnet = {}
+        for key, value in state_dict.items():
+            new_key = key \
+                .replace(f"model.1.sub.{self.num_block}.", "conv_body.") \
+                .replace("model.0"    , "conv_first") \
+                .replace("model.1.sub", "body") \
+                .replace(".0.weight"  , ".weight") \
+                .replace(".0.bias"    , ".bias") \
+                .replace(".RDB1."     , ".rdb1.") \
+                .replace(".RDB2."     , ".rdb2.") \
+                .replace(".RDB3."     , ".rdb3.")
+            if self.num_block == 23:
+                new_key = new_key \
+                .replace("model.3."   , "conv_up1.") \
+                .replace("model.6."   , "conv_up2.") \
+                .replace("model.8."   , "conv_hr.") \
+                .replace("model.10."  , "conv_last.")
+            else:
+                new_key = new_key \
+                .replace("model.3."   , "conv_up1.") \
+                .replace("model.5."   , "conv_hr.") \
+                .replace("model.7."   , "conv_last.") \
+                .replace("model.8."   , "conv_last.")
+            loadnet[new_key] = value
+        super().load_state_dict(loadnet, strict=strict)
+
     def forward(self, x):
-        if self.scale == 2:
-            feat = pixel_unshuffle(x, scale=2)
-        elif self.scale == 1:
-            feat = pixel_unshuffle(x, scale=4)
+        if self.is_real_esrgan and self.scale != 4:
+            if self.scale == 2:
+                feat = pixel_unshuffle(x, scale=2)
+            elif self.scale == 1:
+                feat = pixel_unshuffle(x, scale=4)
         else:
             feat = x
         feat = self.conv_first(feat)
@@ -114,6 +151,7 @@ class RRDBNet(nn.Module):
         feat = feat + body_feat
         # upsample
         feat = self.lrelu(self.conv_up1(F.interpolate(feat, scale_factor=2, mode='nearest')))
-        feat = self.lrelu(self.conv_up2(F.interpolate(feat, scale_factor=2, mode='nearest')))
+        if self.conv_up2:
+            feat = self.lrelu(self.conv_up2(F.interpolate(feat, scale_factor=2, mode='nearest')))
         out = self.conv_last(self.lrelu(self.conv_hr(feat)))
         return out
